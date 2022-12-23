45b1aa792b94438d6a51bc0985be3829ec9fba03
amirhosein <amirhoseinh1373@gmail.com>
2022-12-23 16:52:05 +0330

    init
    get list of 52 cards

---
+++ deck.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i+1, card)
+	}
+}
+
+func listOfCards() deck {
+
+	var cards deck
+
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	cards := listOfCards()
+
+	cards.print()
+}
